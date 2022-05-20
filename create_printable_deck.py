1034e2f72f5e85b246503fe584082c4238135bfe
Frank Murray <fmurray1@mix.wvu.edu>
2022-05-20 16:28:48 -0500

    added threading, added a progress bar, fixed a few bugs

--- create_printable_deck.py
+++ create_printable_deck.py
@@ -2,35 +2,49 @@
 from typing import Tuple
 import requests
 import click
+import progressbar
 
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
 from os import walk, pardir, path
 from bs4 import BeautifulSoup
 from reportlab.lib.pagesizes import LEGAL
 from reportlab.lib.units import inch
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Image
-from math import ceil
+from math import ceil, floor
 from os import mkdir, chdir
 
 
 URL = 'https://scryfall.com/search?q='
 
 successful_images = []
+prog_bar = None
+
+mutex = Lock()
 
 @click.command()
 @click.option('--card_path','-p', type=click.Path(exists=True), required=True)
 @click.option('--deck_name','-d', type=click.Path(exists=False), required=True)
 def main(card_path, deck_name):
+    global prog_bar
+    global mutex
+
     with open(card_path, 'r') as file:
         mkdir(deck_name)
         chdir(deck_name)
-        for line in file.readlines():
-            #print(f'Getting img for {line}')
-            line = line.strip()
-            if line:
-                get_images_for_card(line)
+        cards_to_get = file.readlines()
+        prog_bar = progressbar.ProgressBar(maxval=len(cards_to_get)*2)
+    prog_bar.start()
+    cards_to_get = map(str.strip, cards_to_get)
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(get_images_for_card, cards_to_get)
     
     make_pdf(deck_name)
+    mutex.acquire()
+    prog_bar.finish()
+    mutex.release()
 
 
 def get_page(url) -> None:
@@ -39,6 +53,8 @@ def get_page(url) -> None:
 
 def download_img_to_file(name, img_src):
     global successful_images
+    global prog_bar
+    global mutex
 
     sanitized_name = ''.join(name.split()).replace('.', '').replace(',', '')
 
@@ -52,18 +68,23 @@ def download_img_to_file(name, img_src):
         jpg.write(img_response.content)
 
     successful_images.append(sanitized_name+'.jpg')
+    
+    mutex.acquire()
+    prog_bar.update(prog_bar.currval+1)
+    mutex.release()
 
 def get_img_src(name, class_name):
     soup = get_page(URL+f'"{name}"')
     card_image_div = soup.find('div', {'class', class_name})
     if not card_image_div:
-        cards = soup.findAll('a', {'class', class_name})
+        cards = soup.findAll('a', {'class', 'card-grid-item-card'})
         for card in cards:
             if card.find('span', {'class', 'card-grid-item-invisible-label'}).get_text().lower() == name.lower():
                 soup = get_page(card['href'])
                 card_image_div = soup.find('div', {'class', class_name})
     if not card_image_div:
-        print(f'could not find card_image_div for {name}')
+        if "front" in class_name:
+            print(f'could not find card_image_div for {name}')
         return None
     img_sec = card_image_div.find('img')
     if not img_sec:
@@ -86,10 +107,15 @@ def get_images_for_card(name):
     back_src = get_image_back(name)
     if back_src is not None:
         download_img_to_file(name+"_back", back_src)
-    download_img_to_file(name, front_src)
+    if front_src is not None:
+        download_img_to_file(name, front_src)
+    else:
+        print(f'could not find img for {name}')
     
 def make_pdf(deck_name):
     global successful_images
+    global prog_bar
+    global mutex
     c = Canvas(f'{deck_name}.pdf', pagesize=LEGAL)
     num_pages =  ceil(len(successful_images)/9)
     if num_pages <= 0:
@@ -115,6 +141,17 @@ def make_pdf(deck_name):
                 y_align = 7.75
             img_name = successful_images[i+(current_page*9)]
             c.drawImage(img_name, x_align*inch, y_align*inch, width=2.5*inch, height=3.48*inch)
+        
+        # To explain:
+        # Downloading the images is 50% of the work because our "max" is 2* number of images, 
+        # therefore we need to figure out how much of the overall "max" value our pages take up
+        # we do this by taking half the max value i.e., 50% and then adding the ammount equivalent to
+        # half of the percentage of the pages we are done with  
+        mutex.acquire()
+        additional_percent = (current_page/num_pages)/2
+        new_value = floor((prog_bar.maxval/2)+(additional_percent*prog_bar.maxval))
+        prog_bar.update(new_value)
+        mutex.release()
         c.showPage()
     c.save()
         
